6346cf917a9032ba714f3b2154b7fdb9b2dde451
Githubpach <pachalokibombwe@gmail.com>
2026-01-14 23:04:16 +0200

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'https://voicecommand-business-assistant.vercel.app/'
+  origin: 'https://voice-commandbusiness-assistant.vercel.app/'
 }));
 app.use(express.json());
 
